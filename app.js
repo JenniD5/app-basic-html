b6fddea43f415c7db7bbf4d080329c265ebd70f3
JenniD5 <elizabethduran0105@gmail.com>
2019-08-28 23:01:58 -0700

    esta vez funcionan todas las notas

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const buttons = document.querySelectorAll('button');//para que busque todos los botones 
 
+
+//const keyNoteDown = event =>
+//{
+    ///console.log(event);//para que imprima el evento 
+    //const key= event.key;
+    //const button=document.querySelector(`button[data-key="${key}"]`)//los queryselector buscn etiquetas, en este cao que se equivalente al key 
+    //con [] puedes buscar todo lo que tenga adentro 
+     //if(button) button.click(); //para cuando presiones un boton que no es o que no tiene funcion
+//}
+//document.addEventListener(`keydown`, keyNoteDown );//kateDown nombre del evento para buscar una tecla 
+const buttons = document.querySelectorAll('button');
 
 const playnote = event =>{
     const button = event.target;
@@ -15,20 +25,18 @@ const playnote = event =>{
 }
 
 buttons.forEach(
-    button => button.addEventListener('click', playnote)
-   
+    button => button.addEventListener('click', playnote)  
 );
 
-const keyNoteDown = event =>
-{
-    console.log(event);//para que imprima el evento 
-    const key= event.key;
-    const button=document.querySelector(`button[data-key="${key}"]`)//los queryselector buscn etiquetas, en este cao que se equivalente al key 
-    //con [] puedes buscar todo lo que tenga adentro 
-     if(button) button.click(); //para cuando presiones un boton que no es o que no tiene funcion
+
+const keyNoteDown = event =>{
+    //console.log(event);
+    const key = event.key;
+    console.log(key);
+    const button = document.querySelector(`button[data-key="${key}"]`)
+    if(button) button.click();
 }
-document.addEventListener(`keydown`, keyNoteDown );//kateDown nombre del evento para buscar una tecla 
 
-
+document.addEventListener('keydown', keyNoteDown);
 
 
